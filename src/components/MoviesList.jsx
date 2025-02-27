d18e2d4b7ce1a02a2da86a47feeac0359b2fedcb
aryamishra21 <aryamishra212001@gmail.com>
2025-02-27 18:15:05 +0530

    lang update

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { PosterURL } from '../utils/constants'
 import { Link } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { addMoviePageData } from '../utils/store/moviesSlice'
+import { lang } from '../utils/languageConstants'
 
 const MoviesList = ({title,movies}) => {
+  const language=useSelector(store=>store.config.lang)
+  console.log(lang[language][title],'lang')
   const dispatch=useDispatch()
     console.log(title,movies)
   return (
     <div className=' w-full text-white relative '>
-        <p className='font-semibold text-lg mb-8'>{title}</p>
+        {/* <p className='font-semibold text-lg mb-8'>{title}</p> */}
+        <p className='font-semibold text-lg mb-8'>{lang[language][title]}</p>
         <div className='flex gap-6 overflow-x-scroll w-full noScrollBar mb-5 '>
             {movies?.map((movie)=>movie?.poster_path && <Link onClick={()=>dispatch(addMoviePageData(movie))} className='min-w-[10rem] h-[15rem] overflow-hidden ' to={'/movie?q='+movie.id}><img src={PosterURL+ movie?.poster_path} alt="" className='w-[10rem] h-full  hover:scale-[1.2] transition-all duration-300 object-cover' /></Link>)}
         </div>
@@ -17,4 +21,5 @@ const MoviesList = ({title,movies}) => {
   )
 }
 
+// {lang[language].search}
 export default MoviesList
--- src/utils/languageConstants.jsx
+++ src/utils/languageConstants.jsx
@@ -1,14 +1,26 @@
 export const lang={
     en:{
         search:'Search',
-        gptSearchPlaceholder:'What would you like to watch today?'
+        gptSearchPlaceholder:'What would you like to watch today?',
+        'Now Playing':'Now Playing',
+        'Top Rated':'Top Rated',
+        'Popular':'Popular',
+        'Upcoming':'Upcoming'
     },
     hindi:{
         search:'खोज',
-        gptSearchPlaceholder:'आज आप क्या देखना चाहेंगे?'
+        gptSearchPlaceholder:'आज आप क्या देखना चाहेंगे?',
+        'Now Playing':'अभी चल रही है',
+        'Top Rated':'टॉप रेटेड',
+        'Popular':'लोकप्रिय',
+        'Upcoming':'आगामी'
     },
     spanish:{
         search:'Buscar',
-        gptSearchPlaceholder:'¿Qué te gustaría ver hoy?'
+        gptSearchPlaceholder:'¿Qué te gustaría ver hoy?',
+        'Now Playing':'Ahora jugando',
+        'Top Rated':'Mejor calificado',
+        'Popular':'Popular',
+        'Upcoming':'Próxima'
     }
 }
